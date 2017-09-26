bfbf046b22976d75a8501001b1d303d58d01f840
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-09-26 15:04:28 -0700

    Added reset game function

--- index.py
+++ index.py
@@ -7,7 +7,6 @@ def load_word():
 
    words_list = words_list[0].split(' ')
    secret_word = random.choice(words_list)
-   # secret_letters = list(secret_word)
    return secret_word
 
 def is_word_guessed(secret_word, current_guess, word_reveal):
@@ -37,20 +36,27 @@ def get_guessed_word(secret_word, letters_guessed):
       yet been guessed.
     '''
 
-    # I don't think this function is necessary...
+    # This function is unnecessary; letters can be visually represented otherwise.
 
-    """available_letters = list("abcdefghijklmnopqrstuvwxyz")
-    for i in range(len(letters_guessed)):
-        if (letters_guessed[i] is in available_letters):
-            """
+def reset_game():
+    reset_parameter = raw_input("Dare to tempt fate again? (Y/N)   ")
+
+    if reset_parameter.lower().startswith("y"):
+        print "\nInteresting choice...\n"
+        hangman(load_word())
+    elif reset_parameter.lower().startswith("n"):
+        print "\nFly, you fool...\n"
+        exit()
+    else:
+        print "\nIt seems the noose robbed your voice away, eh? Try again...\n"
+        reset_game()
 
 def hangman(secret_word):
 
     wrong_guesses = 0
     letters_guessed = ""
 
-    print "Guess letters and try not to hang yourself! You have six (6) wrong guesses!"
-    # print "There are " + str(len(secret_word) - int(letters_guessed)) + " letters left!"
+    print "\nGuess letters and try not to hang yourself! You have six (6) wrong guesses!\n"
 
     word_reveal = get_guessed_word(secret_word, letters_guessed)
     print "The secret word has " + str(len(secret_word)) + " letters. Dare to guess?\n"
@@ -59,36 +65,32 @@ def hangman(secret_word):
         current_guess = raw_input("Enter a letter here: ")
 
         if (current_guess in secret_word) and (current_guess not in letters_guessed):
-            print "Good guess! One step closer to victory and freedom!\n"
+            print "\nGood guess! One step closer to victory and freedom!\n"
             letters_guessed += current_guess + ". "
-            print "Current Progress: " + is_word_guessed(secret_word, current_guess, word_reveal) #This will probably not work
+            print "Current Progress: " + is_word_guessed(secret_word, current_guess, word_reveal) + "\n"
 
 
         elif (current_guess not in secret_word) and (current_guess not in letters_guessed) and (wrong_guesses <= 5):
             wrong_guesses += 1
-            print "Bad guess! One step closer to failure and death...\n"
+            print "\nBad guess! One step closer to failure and death...\n"
             letters_guessed += current_guess + ". "
-            print "Current Progress: " + is_word_guessed(secret_word, current_guess, word_reveal) #This will probably not work
+            print "Current Progress: " + is_word_guessed(secret_word, current_guess, word_reveal) + "\n"
 
         else:
-            print "Poor guess. Try again and pick a letter you haven't already picked!\n"
-            print "Current Progress: " + is_word_guessed(secret_word, current_guess, word_reveal) #This will probably not work
+            print "\nPoor guess. Try again and pick a letter you haven't already picked!\n"
+            print "Current Progress: " + is_word_guessed(secret_word, current_guess, word_reveal) + "\n"
 
         if "".join(word_reveal) == secret_word:
-            print "Good job escaping the hangman's noose!\n"
-            print "You win!"
-            break
+            print "\nGood job escaping the hangman's noose! It seems fate favors you today."
+            print "You win! But perhaps fate has more in store for you...\n"
+            reset_game()
+            # break
 
     if wrong_guesses == 6:
         print "The noose tightens around your neck and darkness consumes you."
         print "You have failed! But fate may save you yet...\n"
-        reset_game = raw_input("Dare to try again? ")
+        reset_game()
 
-        if (reset_game == "y") or (reset_game == "Y") or (reset_game == "yes") or (reset_game == "YES"):
-            print "\nInteresting choice...\n"
-            hangman(load_word())
-        elif (reset_game == "n") or (reset_game == "N") or (reset_game == "no") or (reset_game == "NO"):
-            print "\nBetter luck next time...\n"
 
 
 secret_word = load_word()
