b5d07172a34dd6bc96beea648d7a172d72e7db54
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-09-26 16:01:29 -0700

    Fixed some minor styling issues

--- index.py
+++ index.py
@@ -22,8 +22,8 @@ def update_word(secret_word, current_guess, word_reveal):
 
     # Loop that iterates through play word's length and updates guessed letters
     while i < len(secret_word):
-        if current_guess == secret_word[i]:
-            word_reveal[i] = current_guess
+        if current_guess.lower() == secret_word[i]:
+            word_reveal[i] = current_guess.lower()
         i += 1
 
     # Returns joined string of guessed and empty letter values
@@ -70,6 +70,11 @@ def reset_game():
         reset_game()
 
 
+# Function to generate and display helpful diagrams for current game state
+# def game_image(wrong_guesses):
+    # 
+
+
 # Main function to initialize, start, and play hangman game
 def hangman(secret_word):
 
@@ -77,9 +82,9 @@ def hangman(secret_word):
     letters_guessed = ""
 
     # Introductory statement to teach rules to new player
-    print "\nYou have been sentenced to death by hanging. But perhaps fate does not wish you dead today... \n"
-    print "Your only hope is guessing the letters of the secret word before you hang!\n"
-    print "Guess the letters and try not to get youreself killed! After six (6) incorrect guesses, your fate is sealed!\n"
+    print "\n>> You have been sentenced to death by hanging. But perhaps fate does not wish you dead today... \n"
+    print ">> Your only hope is guessing the letters of the secret word before you hang!\n"
+    print ">> Guess the letters and try not to get youreself killed! After six (6) incorrect guesses, your fate is sealed!\n"
 
     # Reveal to player the length of the secret word
     word_reveal = get_guessed_word(secret_word, letters_guessed)
@@ -88,29 +93,34 @@ def hangman(secret_word):
     # Loop to allow for six wrong guesses
     while wrong_guesses < 6:
         # Print guessed letters so far and accept user input for letter guess
-        print "Your Guessed Letters: " + letters_guessed + "\n"
         current_guess = raw_input("Enter a letter here: ")
 
         # If guessed letter is valid and not duplicate, add to guessed letters and notify user of successful guess
-        if (current_guess in secret_word) and (current_guess not in letters_guessed):
-            print "\nGood guess! One step closer to victory and freedom!\n"
-            letters_guessed += current_guess + ". "
+        if (current_guess.lower() in secret_word) and (current_guess.lower() not in letters_guessed):
+            print "\n******************************"
+            print "\nGOOD guess! One step closer to victory and freedom!\n"
+            letters_guessed += current_guess.lower() + ". "
+            print "Your Guessed Letters: " + letters_guessed + "\n"
             print "Current Progress: " + update_word(secret_word, current_guess, word_reveal) + "\n"
 
         # If guessed letter is invalid and not duplicate, add to guessed letters, iterate wrong guesses, and notify user of unsuccessful guess
-        elif (current_guess not in secret_word) and (current_guess not in letters_guessed) and (wrong_guesses <= 5):
+        elif (current_guess.lower() not in secret_word) and (current_guess.lower() not in letters_guessed) and (wrong_guesses <= 5):
             wrong_guesses += 1
-            print "\nBad guess! One step closer to failure and death...\n"
-            letters_guessed += current_guess + ". "
+            print "\n******************************"
+            print "\nBAD guess! One step closer to failure and death...\n"
+            letters_guessed += current_guess.lower() + ". "
+            print "Your Guessed Letters: " + letters_guessed + "\n"
             print "Current Progress: " + update_word(secret_word, current_guess, word_reveal) + "\n"
 
         # If guessed letter is a duplicate or not a letter, scold user and notify them to try again
         else:
-            print "\nPoor guess. Try again and pick a letter you haven't already picked!\n"
+            print "\nPOOR guess. Try again and pick a letter you haven't already picked!\n"
+            print "Your Guessed Letters: " + letters_guessed + "\n"
             print "Current Progress: " + update_word(secret_word, current_guess, word_reveal) + "\n"
 
         # If user has guessed all letters correctly without losing, notify them of game win and prompt a game reset
         if "".join(word_reveal) == secret_word:
+            print "\n******************************"
             print "\nGood job escaping the hangman's noose! It seems fate favors you today."
             print "You win! But perhaps fate has more in store for you...\n"
             reset_game()
@@ -118,7 +128,8 @@ def hangman(secret_word):
 
     # If user has guessed too many invalid guesses and lost, notify them of game loss and prompt a game reset
     if wrong_guesses == 6:
-        print "The noose tightens around your neck and darkness consumes you."
+        print "\n******************************"
+        print "\nThe noose tightens around your neck and darkness consumes you."
         print "You have failed! But fate may save you yet...\n"
         reset_game()
 
